a4e596a83271f591083d52467698bb1881d6c76f
Tusharshrivastav <ts3657@gmail.com>
2021-05-17 14:10:15 +0530

    add book action

---
+++ client/src/redux/actions/books/bookActions.js
@@ -0,0 +1,25 @@
+import {
+  CREATE_BOOK_FAIL,
+  CREATE_BOOK_REQUEST,
+  CREATE_BOOK_SUCCESS,
+} from "../actionTypes";
+import axios from "axios";
+export const createBookAction = (bookData) => async (dispatch) => {
+  try {
+    dispatch({ type: CREATE_BOOK_REQUEST });
+    const config = {
+      "Content-type": "application/json",
+    };
+    const { data } = await axios.post("/api", bookData, config);
+
+    dispatch({
+      type: CREATE_BOOK_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: CREATE_BOOK_FAIL,
+      payload: error.response && error.response.data.message,
+    });
+  }
+};
